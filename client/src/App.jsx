62143924abb0348ca9e918519f2447bc14446c5c
A-Vaughn <akilvaughn14@gmail.com>
2024-05-04 01:40:30 -0400

    added React w/Tailwind

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <div className='m-5'>
+      <h1 className='text-amber-500 text-6xl text-center'>Welcome to the Book Shop</h1>
+    </div>
+  )
+}
+
+export default App
